05c163499a853c28a02c10d50e7a2d12ad23d277
michaelNorrisDollarShaveClub <michael.norris@dollarshaveclub.com>
2017-12-14 16:37:52 -0800

    Add minibox formula

---
+++ Formula/minibox.rb
@@ -0,0 +1,28 @@
+class  Minibox < Formula
+  desc "Minibox CLI"
+  homepage "https://github.com/dollarshaveclub/minibox"
+  url "git@github.com:dollarshaveclub/minibox.git",
+      :using => :git,
+      :tag => "v0.1.0",
+      :revision => "b8b2fc1f20fe90130aa5518dee613621f04b617b"
+
+  depends_on "go" => :build
+
+  def install
+
+    ENV["GOPATH"] = buildpath
+    miniboxpath = buildpath/"src/github.com/dollarshaveclub/minibox"
+    miniboxpath.install buildpath.children
+
+    cd miniboxpath do
+      system "make"
+      bin.install "bin/minibox"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system "#{bin}/minibox"
+    assert true
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # homebrew-public
+
+Homebrew tap for public DSC formulas and casks
+
+## Add this repo as a tap
+
+`brew tap dollarshaveclub/public`
+
+## Install a formula
+
+`brew install minibox` 
