f9e551b0b4f5a03d2f1559b86167ef1ed19b9dda
Benjamen Keroack <benjamen@dollarshaveclub.com>
2018-02-06 13:08:45 -0800

    updated for release 0.3.5-TESTING

--- Formula/minibox.rb
+++ Formula/minibox.rb
@@ -3,12 +3,20 @@ class  Minibox < Formula
   homepage "https://github.com/dollarshaveclub/minibox"
   url "git@github.com:dollarshaveclub/minibox.git",
       :using => :git,
-      :tag => "v0.3.0",
-      :revision => "0b409cf299144bf514841dfd3cd03094cc351b6d"
+      :tag => "0.3.5-TESTING",
+      :revision => "c9e06ecebd2127ebdd1b63234b9df4569bfda2ca"
   revision 1
   head "git@github.com:dollarshaveclub/minibox.git", :using => :git
   depends_on "go" => :build
   depends_on "kubectl"
+
+  
+  bottle do
+    root_url "https://github.com/dollarshaveclub/minibox/releases/download/0.3.5-TESTING/"
+    rebuild 1
+    sha256 "0723630a76ac7755c6ffea160521f7764bafc8fb1e11b69b2055d6387e47e683" => :el_capitan
+    sha256 "0723630a76ac7755c6ffea160521f7764bafc8fb1e11b69b2055d6387e47e683" => :high_sierra
+    sha256 "0723630a76ac7755c6ffea160521f7764bafc8fb1e11b69b2055d6387e47e683" => :sierra
 
   def install
 
