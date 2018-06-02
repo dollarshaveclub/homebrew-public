a18ebeab670fa616ee1a557f5a1930fe6ba6a22d
Joel Esterline <joel.esterline@gmail.com>
2018-06-01 17:09:33 -0700

    updated for release v0.5.0

--- Formula/minibox.rb
+++ Formula/minibox.rb
@@ -4,8 +4,8 @@ class  Minibox < Formula
   homepage "https://github.com/dollarshaveclub/minibox"
   url "git@github.com:dollarshaveclub/minibox.git",
       :using => :git,
-      :tag => "v0.4.0",
-      :revision => "dd33454ceced44d9310e279f16683c3f6f269b10"
+      :tag => "v0.5.0",
+      :revision => "b945965ba3d1aac4b7af43977cee8ed4293c94ac"
   revision 1
   head "git@github.com:dollarshaveclub/minibox.git", :using => :git
   depends_on "go" => :build
@@ -13,11 +13,11 @@ class  Minibox < Formula
 
   
   bottle do
-    root_url "https://github.com/dollarshaveclub/minibox/releases/download/v0.4.0/"
+    root_url "https://github.com/dollarshaveclub/minibox/releases/download/v0.5.0/"
     rebuild 1
-    sha256 "40cfa16c89d3f7bd95757ead773cbac96606e8ebca04bfa73be62aa6bbc680bb" => :el_capitan
-    sha256 "40cfa16c89d3f7bd95757ead773cbac96606e8ebca04bfa73be62aa6bbc680bb" => :high_sierra
-    sha256 "40cfa16c89d3f7bd95757ead773cbac96606e8ebca04bfa73be62aa6bbc680bb" => :sierra
+    sha256 "33da934f4ed6dc00403b22f19835520bd187797f051775e61d6b0fe32aefccaf" => :el_capitan
+    sha256 "33da934f4ed6dc00403b22f19835520bd187797f051775e61d6b0fe32aefccaf" => :high_sierra
+    sha256 "33da934f4ed6dc00403b22f19835520bd187797f051775e61d6b0fe32aefccaf" => :sierra
   end
   
 
