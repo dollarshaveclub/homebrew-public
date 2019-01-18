a4ca446539b702cde10db274a008785e5033a82d
goreleaserbot <goreleaser@dollarshaveclub.com>
2019-01-17 19:24:43 -0800

    Brew formula update for vault-shared-users version v0.0.1

--- Formula/vault-shared-users.rb
+++ Formula/vault-shared-users.rb
@@ -1,11 +1,10 @@
-require_relative "../custom_download_strategy"
-
+require_relative "../custom_download_strategy.rb"
 class VaultSharedUsers < Formula
   desc "Easily share users with 2FA access through Hashicorp Vault"
   homepage ""
   url "https://github.com/dollarshaveclub/vault-shared-users/releases/download/v0.0.1/vault-shared-users_0.0.1_Darwin_x86_64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   version "0.0.1"
-  sha256 "e47b285a42ef2e15eaf6a93bc88e7d46c2cea9be1b99c47401b86b28fa995e5a"
+  sha256 "c19ccd90a300fd178835b2b6df7ad07ead2ca099457e121904f05173d4e2d55c"
 
   def install
     bin.install "vault-shared-users"
