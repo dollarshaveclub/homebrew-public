72068d6a21bbb1cf23ac2c11f9a63b765dac8e06
goreleaserbot <goreleaser@dollarshaveclub.com>
2019-02-01 12:05:00 -0800

    Brew formula update for acyl version v0.6.0

--- Formula/acyl.rb
+++ Formula/acyl.rb
@@ -2,9 +2,9 @@ require_relative "../custom_download_strategy.rb"
 class Acyl < Formula
   desc "Testing Environments On Demand"
   homepage ""
-  url "https://github.com/dollarshaveclub/acyl/releases/download/v0.6.0-rc1/acyl_0.6.0-rc1_MacOS_x86_64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.6.0-rc1"
-  sha256 "4e01b4965190894e6502d299af9afd967dd59b73c7c922df2c75f85bd8f6e207"
+  url "https://github.com/dollarshaveclub/acyl/releases/download/v0.6.0/acyl_0.6.0_MacOS_x86_64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.6.0"
+  sha256 "5eed6f7db3bb31dd060ae0239d3fe05243afca226c43ef40901ccedf6f1c83b3"
 
   def install
     bin.install "acyl"
