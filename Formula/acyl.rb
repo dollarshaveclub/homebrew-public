b3694cbf099661d53c0a9307f9e2ef715f442b20
goreleaserbot <goreleaser@dollarshaveclub.com>
2020-12-11 18:31:36 -0600

    Brew formula update for acyl version v0.8.0

--- Formula/acyl.rb
+++ Formula/acyl.rb
@@ -2,17 +2,16 @@
 class Acyl < Formula
   desc "Testing Environments On Demand"
   homepage ""
-  version "0.7.6"
+  version "0.8.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dollarshaveclub/acyl/releases/download/v0.7.6/acyl_0.7.6_MacOS_x86_64.tar.gz"
-    sha256 "d0de9e20340d1bd51e8cc3549b12307730b6dada8a26bb1f2a26950224fa16b5"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/dollarshaveclub/acyl/releases/download/v0.7.6/acyl_0.7.6_Linux_x86_64.tar.gz"
-      sha256 "28b3bcf4ad68054895afe69d39d4e126fc32057cc210568b5c9b4f819b74aa95"
-    end
+    url "https://github.com/dollarshaveclub/acyl/releases/download/v0.8.0/acyl_0.8.0_MacOS_x86_64.tar.gz"
+    sha256 "2eecc150150d9ef60c1e5b3bee334300716369b6b76a923f82aa70fb596a2325"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/dollarshaveclub/acyl/releases/download/v0.8.0/acyl_0.8.0_Linux_x86_64.tar.gz"
+    sha256 "d70f97e6992764d52742d3f7fc44f36ba408e92b199a3dfc0b6d1534f84a918a"
   end
 
   def install
