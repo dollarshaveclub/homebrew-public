63981458f3cf90231a3afb1169b1b27c6df02fa5
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-05 22:23:55 -0400

    Brew formula update for thermite version v0.0.23

--- Formula/thermite.rb
+++ Formula/thermite.rb
@@ -5,29 +5,29 @@
 class Thermite < Formula
   desc "Removes old Amazon Elastic Container Registry images that are not deployed in a Kubernetes cluster"
   homepage "https://github.com/dollarshaveclub/thermite"
-  version "0.0.21"
+  version "0.0.23"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.21/thermite_0.0.21_darwin_amd64.tar.gz"
-      sha256 "2b108df44c95fe96bde0e2fb188afd70502d33b5e6292ea375f2d4c445ea44fc"
+      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.23/thermite_0.0.23_darwin_amd64.tar.gz"
+      sha256 "d8d443a2cf5c2cc721749dd7cbe66226f11f751fd9eb73ba2e10c25583e94ffa"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.21/thermite_0.0.21_darwin_arm64.tar.gz"
-      sha256 "9a1bfbf89b929d4e17bd44e328a6c55180f38fa718424df25e655cec0d3c312c"
+      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.23/thermite_0.0.23_darwin_arm64.tar.gz"
+      sha256 "204b8715a337063b9a54a7dd8c86da26eba74a2ba78e808b5e70bb99404dfbee"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.21/thermite_0.0.21_linux_amd64.tar.gz"
-      sha256 "cd14f370b52a7cf6493ca016a66f3277432a9e1c4d79a01f14a983ca055f962d"
+      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.23/thermite_0.0.23_linux_amd64.tar.gz"
+      sha256 "59a4cd4bc8810b8d72985b68c1bfdfcc56689b09258187dfaa54ac59aae60dc7"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.21/thermite_0.0.21_linux_arm64.tar.gz"
-      sha256 "9c786cd50606fa6cd892b49724605b3114107bea943224e3751e50c25b0385d5"
+      url "https://github.com/dollarshaveclub/thermite/releases/download/v0.0.23/thermite_0.0.23_linux_arm64.tar.gz"
+      sha256 "8d0ac1edfd2c139c4a195cdfeef9617a8c629bba5475875ebd156bceaf5c70b8"
     end
   end
 
