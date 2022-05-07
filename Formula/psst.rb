99f08ed54a44416c9faabaf522ac5ee232e271e5
Brian McKelvey <theturtle32@gmail.com>
2022-05-06 17:06:51 -0700

    Updating psst formula for Apple Silicon

--- Formula/psst.rb
+++ Formula/psst.rb
@@ -1,21 +1,12 @@
-# GENERATED FROM TEMPLATE. DO NOT EDIT!
 class  Psst < Formula
   desc "Secrets distribution"
   homepage "https://github.com/dollarshaveclub/psst"
   url "git@github.com:dollarshaveclub/psst.git",
       using:    :git,
-      tag:      "v0.2.0",
-      revision: "67eca859aae1074f8e141e377a7132d6b5e4e2fa"
+      tag:      "v0.2.1",
+      revision: "afada9fa109b4ed23d4a9541001c329eb5cd72eb"
 
   head "git@github.com:dollarshaveclub/psst.git", using: :git
-
-  bottle do
-    root_url "https://github.com/dollarshaveclub/psst/releases/download/v0.2.0"
-    rebuild 1
-    sha256 el_capitan:  "9ee65127bc27c596929474e1be95d4ec821e4771049d2aec74a1dcc7027942a5"
-    sha256 high_sierra: "9ee65127bc27c596929474e1be95d4ec821e4771049d2aec74a1dcc7027942a5"
-    sha256 sierra:      "9ee65127bc27c596929474e1be95d4ec821e4771049d2aec74a1dcc7027942a5"
-  end
 
   depends_on "go" => :build
 
